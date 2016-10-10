a013ef2ccb6ce263e5e2adbc1fa547076db15cbf
ujs <ujalashanker@gmail.com>
2016-10-10 16:55:55 +0530

    sqr root program test

---
+++ square-root.py
@@ -0,0 +1,27 @@
+# Compute square root of given number
+
+x = 63
+i = 1
+
+if x < 0:
+        print("can't find squareroot of negative number")
+
+else:
+    
+
+    while i * i < x:
+        i = i + 1
+    if i * i == x:
+        print (i)
+    else:
+        print('not perfect square')
+
+    
+
+
+
+    
+
+    
+
+    
