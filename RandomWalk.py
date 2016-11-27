91b21323cfe208b9ba88850e361ea25e4fe3b4d1
ujs <ujalashanker@gmail.com>
2016-11-27 20:07:26 +0530

    random walk

---
+++ RandomWalk.py
@@ -0,0 +1,15 @@
+import math, random, pylab
+
+class Location(object):
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+    def move(self, xc, yc):
+        return Location(self.x+float(xc), self.y+float(yc))
+    def getCoords(self):
+        return self.x, self.y
+    def getDist(self, other):
+        ox, oy = other.getCoords()
+        xDist = self.x - ox
+        yDist = self.y - oy
+        return math.sqrt(xDist**2 + yDist**2) 
