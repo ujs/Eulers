80ed57f9ef45a0d528c8270364837cc0ff612a61
ujs <ujalashanker@gmail.com>
2016-12-17 19:32:44 +0530

    tz

--- Import_jason.py
+++ Import_jason.py
@@ -8,3 +8,20 @@ time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 
 
 
+def tz_counts(sequence):
+    count = {}
+    for i in sequence:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+
+    return count
+
+count_dict = tz_counts(time_zones)
+
+def top_counts(count_dict,n=10):
+    value_key_pair = [(count,tz) for tz, count in count_dict.items()]
+    value_key_pair.sort()
+    return value_key_pair[-n:]
+
