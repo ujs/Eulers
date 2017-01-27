7eb65c075ed16992c90827879c62c3158e2072a0
ujs <ujalashanker@gmail.com>
2017-01-27 20:55:22 +0530

    numpy4_ufunc

---
+++ ds/numpy_234.py
@@ -0,0 +1,33 @@
+import array
+L = list(range(10))
+A = array.array('i', L)
+#print(A)
+
+import numpy as np
+np.array ([1,3,2,4,5])
+
+#Numpy Array Attributes
+np.random.seed(0)
+x1 = np.random.randint(10, size = 6)
+x2 = np.random.randint(10, size=(3, 4))
+x3 = np.random.randint(10, size=(3, 4, 5))
+
+# print("dimension of x3: ",x3.ndim)
+# print("shape of x3: ",x3.shape)
+# print("size of x3: ",x3.size)
+# print("bytesize of x3 item (in bytes): ",x3.itemsize)
+# print("total bytesize of x3 item (in bytes): ",x3.nbytes)
+x3_subarray = x3[0,1:3,:3].copy()
+x3_subarray[1,1]= 200
+
+x1_reshape = x1.reshape((2,3))
+
+#Converting a given array into an array with reciprocals
+np.random.seed(0)
+
+def calculate_reci(values):
+	output = np.empty(len(values))
+	for i in range (len(values)):
+		output[i] = 1.0/values[i]
+	return output
+values = np.random.randint(1,10,size=5)
