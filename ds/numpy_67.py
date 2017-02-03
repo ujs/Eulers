2b43096cee952276cd5c3d79b76a5e452a2d27b9
ujs <ujalashanker@gmail.com>
2017-02-03 13:53:00 +0530

    np

--- ds/numpy_67.py
+++ ds/numpy_67.py
@@ -4,7 +4,7 @@ b = np.arange(3)
 c = b[:,np.newaxis]
 # print(N)
 # print (c)
-print(np.logaddexp(N,c))
+# print(np.logaddexp(N,c))
 #Array Centering
 Y= np.random.random((10, 3))
 Y_mean = Y.mean(0)
