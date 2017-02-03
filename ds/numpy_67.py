f1904b86b1c0c9b650ec6d6e8c89aaa3ff69d26d
ujs <ujalashanker@gmail.com>
2017-02-03 13:57:34 +0530

    np

--- ds/numpy_67.py
+++ ds/numpy_67.py
@@ -6,5 +6,15 @@ c = b[:,np.newaxis]
 # print (c)
 # print(np.logaddexp(N,c))
 #Array Centering
-Y= np.random.random((10, 3))
+Y = np.random.random((10, 3))
 Y_mean = Y.mean(0)
+
+x = np.linspace(0, 5, 50)
+y = np.linspace(0, 5, 50)[:, np.newaxis]
+
+z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.imshow(z, origin='lower', extent=[0, 5, 0, 5], cmap='viridis')
+plt.colorbar();
