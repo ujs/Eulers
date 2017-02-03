58dcd58656771056eebf1e4ae3a0afb1b4c911ff
ujs <ujalashanker@gmail.com>
2017-02-03 13:37:11 +0530

    np

--- ds/numpy_67.py
+++ ds/numpy_67.py
@@ -1,5 +1,2 @@
-# x and y have 50 steps from 0 to 5
-x = np.linspace(0, 5, 50)
-y = np.linspace(0, 5, 50)[:, np.newaxis]
-
-z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+ N = np.ones((3,2))
+ b = np.arange(3)
