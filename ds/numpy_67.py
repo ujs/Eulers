4d1305faeb3a755d8db8338a40fda24129100cb7
ujs <ujalashanker@gmail.com>
2017-02-03 19:03:45 +0530

    np_matplotlib

--- ds/numpy_67.py
+++ ds/numpy_67.py
@@ -13,8 +13,10 @@ x = np.linspace(0, 5, 50)
 y = np.linspace(0, 5, 50)[:, np.newaxis]
 
 z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
-
-%matplotlib inline
+# %matplotlib inline
 import matplotlib.pyplot as plt
+plt.ion
 plt.imshow(z, origin='lower', extent=[0, 5, 0, 5], cmap='viridis')
-plt.colorbar();
+plt.colorbar()
+plt.show()
+
