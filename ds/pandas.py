a7c439435e15e3f08f017392dad7836035f8e1c1
ujs <ujalashanker@gmail.com>
2017-02-13 19:38:05 +0530

    np_fancy4

--- ds/pandas.py
+++ ds/pandas.py
@@ -16,8 +16,11 @@ def selection_sort(x):
 selection_sort(x)
 
 
-In [3]:
+
 def bogosort(x):
     while np.any(x[:-1] > x[1:]):
         np.random.shuffle(x)
     return x
+
+ x = np.array([2, 1, 4, 3, 5])
+np.sort(x)
