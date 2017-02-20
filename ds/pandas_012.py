6910cf2b8f33b79c02a859b78fbf052e8d9a59ff
ujs <ujalashanker@gmail.com>
2017-02-20 16:08:49 +0530

    pd series

--- ds/pandas_012.py
+++ ds/pandas_012.py
@@ -5,3 +5,10 @@ import pandas as pd
 data = pd.series([1,4,6,3,2], index = [a,b,c,d,e])
 # print(data['c'])
 
+
+pop_dict = {'Cal': 38332521,
+                   'Texas': 26448193,
+                   'New York': 19651127,
+                   'Florida': 19552860,
+                   'Illinois': 12882135}
+population = pd.Series(pop_dict)
