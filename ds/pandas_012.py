3604d7c74cab544c89127622529dff145d987fb9
ujs <ujalashanker@gmail.com>
2017-02-21 13:10:52 +0530

    pd series

--- ds/pandas_012.py
+++ ds/pandas_012.py
@@ -35,3 +35,6 @@ pop = pd.Series({'California': 38332521, 'Texas': 26448193,
                  'New York': 19651127, 'Florida': 19552860,
                  'Illinois': 12882135})
 data = pd.DataFrame({'area':area, 'pop':pop})
+
+print(data['pop'])
+print(data['area'])
