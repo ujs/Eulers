5a70db12d644411ba3d6ba49752eb0bdf3ca5df8
ujs <ujalashanker@gmail.com>
2017-02-23 20:51:15 +0530

    pd series

--- ds/pandas_012.py
+++ ds/pandas_012.py
@@ -28,13 +28,13 @@ pd.DataFrame (population, columns = ['population'])
 A = np.zeros(3, dtype=[('A', 'i8'), ('B', 'f8')])
 
 # df dict 
-# area = pd.Series({'California': 423967, 'Texas': 695662,
-#                   'New York': 141297, 'Florida': 170312,
-#                   'Illinois': 149995})
-# pop = pd.Series({'California': 38332521, 'Texas': 26448193,
-#                  'New York': 19651127, 'Florida': 19552860,
-#                  'Illinois': 12882135})
-# data = pd.DataFrame({'area':area, 'pop':pop})
+area = pd.Series({'California': 423967, 'Texas': 695662,
+                  'New York': 141297, 'Florida': 170312,
+                  'Illinois': 149995})
+pop = pd.Series({'California': 38332521, 'Texas': 26448193,
+                 'New York': 19651127, 'Florida': 19552860,
+                 'Illinois': 12882135})
+data = pd.DataFrame({'area':area, 'pop':pop})
 
 # print(data['pop'])
 # print(data['area'])
