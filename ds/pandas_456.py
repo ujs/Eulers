bd22a3591c428f6be90a233e5cb511ff21d67bb7
ujs <ujalashanker@gmail.com>
2017-02-26 18:13:42 +0530

    pd missing vals

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-cali = pd.read_csv('https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/data/california_cities.csv')
-cali.isnull()
-cali.isnull().sum()
-cali
+# cali = pd.read_csv('https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/data/california_cities.csv')
+# cali.isnull()
+# cali.isnull().sum()
+# cali
