89e430d87306625c90bfe19c2c1c782c30115050
ujs <ujalashanker@gmail.com>
2017-02-27 12:17:19 +0530

    pd multiple index

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 
-cali = pd.read_csv('https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/data/california_cities.csv')
-cali.isnull()
-cali.isnull().sum()
-cali
+# cali = pd.read_csv('https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/data/california_cities.csv')
+# cali.isnull()
+# cali.isnull().sum()
+# cali
 
 index = [('California', 2000), ('California', 2010), ('Texas', 2000), ('Texas', 2010)]
 popu = [31000000, 32000000, 20000000, 21000000]
