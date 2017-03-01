5835d7f63683456c0feab1559aef7f128be1929f
ujs <ujalashanker@gmail.com>
2017-03-01 19:20:58 +0530

    pd multiple index

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -11,3 +11,8 @@ popu = [31000000, 32000000, 20000000, 21000000]
 # population = pd.Series(popu, index = index)
 index = pd.MultiIndex.from_tuples(index)
 pop_df.stack()
+pop_df = pd.DataFrame({'total': pop,
+                       'under18': [9267089, 9284094,
+                                   4687374, 4318033,
+                                   5906301, 6879014]})
+pop_df
