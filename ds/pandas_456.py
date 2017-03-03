cd7c3e93cb9cf052928a4cf0a292bc3a6cf6c017
ujs <ujalashanker@gmail.com>
2017-03-03 20:06:58 +0530

    index advanced

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -9,8 +9,8 @@ import pandas as pd
 index = [('California', 2000), ('California', 2010), ('Texas', 2000), ('Texas', 2010)]
 popu = [31000000, 32000000, 20000000, 21000000]
 # population = pd.Series(popu, index = index)
-# index = pd.MultiIndex.from_tuples(index)
-# pop_df.stack()
+index = pd.MultiIndex.from_tuples(index)
+pop_df.stack()
 pop_df = pd.DataFrame({'total': pop,
                        'under18': [9267089, 9284094,
                                    4687374, 4318033,
