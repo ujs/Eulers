94dc3726d5f6a0c0ae89354a5e58649dc0f555ca
ujs <ujalashanker@gmail.com>
2017-03-08 16:19:13 +0530

    multiindex

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -9,10 +9,10 @@ import pandas as pd
 index = pd.MultiIndex.from_arrays([['CA','CA','TX','TX'],[2000,2010,2000,2010]])
 data = pd.DataFrame([1,2,3,4], index = index, columns = ['fig'])
 data.index.names = ['state','year']
-print(data)
+# print(data)
 
 #Multiindex for both rows and columns
-index = pd.MultiIndex.from_tuples(('city1',2000),('city1',2010),('city2',2000),('city2',2010))
-column =pd.MultiIndex.from_tuples(('car1',2000),('car1',2010),('car2',2000),('car2',2010))
+index = pd.MultiIndex.from_tuples([('city1',2000),('city1',2010),('city2',2000),('city2',2010)])
+column =pd.MultiIndex.from_tuples([('car1',2000),('car1',2010),('car2',2000),('car2',2010)])
 # values = np.random.randn(4,4)
 # df = pd.DataFrame(values, index = index, columns = column)
