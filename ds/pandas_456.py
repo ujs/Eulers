d605f1b0a05b0420c00e664da089c6ebe9450c7d
ujs <ujalashanker@gmail.com>
2017-03-08 16:19:29 +0530

    multiindex

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -14,5 +14,5 @@ data.index.names = ['state','year']
 #Multiindex for both rows and columns
 index = pd.MultiIndex.from_tuples([('city1',2000),('city1',2010),('city2',2000),('city2',2010)])
 column =pd.MultiIndex.from_tuples([('car1',2000),('car1',2010),('car2',2000),('car2',2010)])
-# values = np.random.randn(4,4)
-# df = pd.DataFrame(values, index = index, columns = column)
+values = np.random.randn(4,4)
+df = pd.DataFrame(values, index = index, columns = column)
