1a9147335e6337f1016607d6ca6838939a1e7623
ujs <ujalashanker@gmail.com>
2017-03-08 16:04:10 +0530

    multiindex

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -6,6 +6,6 @@ import pandas as pd
 # cali.isnull().sum()
 # cali
 
-index = pd.MultiIndex.from_arrays([['CA','TX'],[2000,2010]])
-# data = pd.DataFrame(np.random.rand(4,1), index = index, columns = ['fig'])
-# print(data)
+index = pd.MultiIndex.from_arrays([['CA','CA','TX','TX'],[2000,2010,2000,2010]])
+data = pd.DataFrame(np.random.rand(4,1), index = index, columns = ['fig'])
+print(data)
