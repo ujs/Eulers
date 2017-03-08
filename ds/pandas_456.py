b1973929589b2d547a3e09c67087a1a495cbbdbb
ujs <ujalashanker@gmail.com>
2017-03-08 15:56:30 +0530

    multiindex

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -13,4 +13,10 @@ popu = [31000000, 32000000, 20000000, 21000000]
 # population = population.reindex(index1)
 # population_df = population.unstack()
 
-pop_2 = pd.DataFrame(popu, index = [['Cal','Texas'],[2000,2010]], columns = ['population'])
+# pop_2 = pd.DataFrame(popu, index = [['Cal','Texas'],[2000,2010]], columns = ['population'])
+data = {('California', 2000): 33871648,
+        ('California', 2010): 37253956,
+        ('Texas', 2000): 20851820,
+        ('Texas', 2010): 25145561,
+        ('New York', 2000): 18976457,
+        ('New York', 2010): 19378102}
