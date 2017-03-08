768ad7e9e8072a49050fa5db3b02d5c1dfeff413
ujs <ujalashanker@gmail.com>
2017-03-08 18:08:22 +0530

    multiindex

--- ds/pandas_456.py
+++ ds/pandas_456.py
@@ -16,3 +16,18 @@ index = pd.MultiIndex.from_tuples([('city1',2000),('city1',2010),('city2',2000),
 column =pd.MultiIndex.from_tuples([('car1',2000),('car1',2010),('car2',2000),('car2',2010)])
 values = np.random.randn(4,4)
 df = pd.DataFrame(values, index = index, columns = column)
+
+# hierarchical indices and columns
+index = pd.MultiIndex.from_product([[2013, 2014], [1, 2]],
+                                   names=['year', 'visit'])
+columns = pd.MultiIndex.from_product([['Bob', 'Guido', 'Sue'], ['HR', 'Temp']],
+                                     names=['subject', 'type'])
+
+# mock some data
+data = np.round(np.random.randn(4, 6), 1)
+data[:, ::2] *= 10
+data += 37
+
+# create the DataFrame
+health_data = pd.DataFrame(data, index=index, columns=columns)
+health_data
