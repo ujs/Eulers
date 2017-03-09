7dd1877803f07e404e4885c0abd01a39f957a71e
ujs <ujalashanker@gmail.com>
2017-03-09 12:17:40 +0530

    join

--- ds/pandas_789.py
+++ ds/pandas_789.py
@@ -18,4 +18,10 @@ class display(object):
                            for a in self.args)
 
 
+df1 = pd.DataFrame({'employee': ['Bob', 'Jake', 'Lisa', 'Sue'],
+                    'group': ['Accounting', 'Engineering', 'Engineering', 'HR']})
+df2 = pd.DataFrame({'employee': ['Lisa', 'Bob', 'Jake', 'Sue'],
+                    'hire_date': [2004, 2008, 2012, 2014]})
+display('df1', 'df2')
+
 
