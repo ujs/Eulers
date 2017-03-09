0bb7f9f5414c935c7cbb5a7359995b3303ef512e
ujs <ujalashanker@gmail.com>
2017-03-09 11:57:45 +0530

    dataframes

--- ds/pandas_789.py
+++ ds/pandas_789.py
@@ -14,19 +14,24 @@ series2 = pd.Series(['a','b','c'], index = [4,5,6])
 new_ser = pd.concat([series1,series2])
 
 ## Display
-class display(df_object):
-	'''Good Display'''
-	template = """<div style="float: left; padding: 10px;">
+class display(object):
+    """Display HTML representation of multiple objects"""
+    template = """<div style="float: left; padding: 10px;">
     <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
     </div>"""
     def __init__(self, *args):
-    	self.args = args
+        self.args = args
         
     def _repr_html_(self):
-        return '\n'.join(self.template.format(a, eval(a)._repr_html_()) for a in self.args)
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
     
     def __repr__(self):
-        return '\n\n'.join(a + '\n' + repr(eval(a)) for a in self.args)
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
+    
+
+
 
 df1 = make_df('AB', [1, 2])
 df2 = make_df('AB', [3, 4])
