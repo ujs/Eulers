b98f2f46191be6088adbcac49b3b5233ae8dc167
ujs <ujalashanker@gmail.com>
2017-03-09 09:13:20 +0530

    dataframes

--- ds/pandas_789.py
+++ ds/pandas_789.py
@@ -12,13 +12,11 @@ class display(df_object):
     <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
     </div>"""
     def __init__(self, *args):
-        self.args = args
+    	self.args = args
         
     def _repr_html_(self):
-        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
-                         for a in self.args)
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_()) for a in self.args)
     
     def __repr__(self):
-        return '\n\n'.join(a + '\n' + repr(eval(a))
-                           for a in self.args)
+        return '\n\n'.join(a + '\n' + repr(eval(a)) for a in self.args)
     
