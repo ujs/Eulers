ecdb41922818441def21b6e3b9ee103ffc6b52bc
ujs <ujalashanker@gmail.com>
2017-03-10 14:39:20 +0530

    time

--- ds/pandas_last.py
+++ ds/pandas_last.py
@@ -1,4 +1,9 @@
 from pandas_datareader import data 
-import matplotlib.pyplot as pyplotimport seaborn; seaborn.set()
+import matplotlib.pyplot as pyplot
+import seaborn; seaborn.set()
 
 google = data.DataReader('GOOG', start = 2010, end = 2017, data_source = 'google' )
+goog.plot(alpha=0.5, style='-')
+goog.resample('BA').mean().plot(style=':')
+goog.asfreq('BA').plot(style='--');
+plt.legend(['input', 'resample', 'asfreq'], loc='upper left')
