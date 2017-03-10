8dddf9b4db90367636512439e200b5891905296b
ujs <ujalashanker@gmail.com>
2017-03-10 14:41:42 +0530

    time

--- ds/pandas_last.py
+++ ds/pandas_last.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn; seaborn.set()
 
 goog = data.DataReader('GOOG', start = 2010, end = 2017, data_source = 'google' )
-goog = good['Close']
+goog = goog['Close']
 goog.plot(alpha=0.5, style='-')
 goog.resample('BA').mean().plot(style=':')
 goog.asfreq('BA').plot(style='--');
