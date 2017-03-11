b78663673bc4e3f2d7445832848e3fc9deb61039
ujs <ujalashanker@gmail.com>
2017-03-11 11:18:32 +0530

    bike_time series

--- ds/matplotlib.py
+++ ds/matplotlib.py
@@ -6,9 +6,12 @@ x = np.linspace(0,10,100)
 y = np.sin(x)
 
 #Contours
+def f(x, y):
+    return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
 x = np.linspace(0, 5, 50)
 y = np.linspace(0, 5, 40)
 
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
-plot.contourf(X,Y,Z,cmap='RdGy')
+plt.contourf(X,Y,Z,20,cmap='RdGy')
